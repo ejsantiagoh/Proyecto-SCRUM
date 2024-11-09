f856e0234f9cde248262f6ea9cd11c6969879ef2
ejsantiagoh <100106824+ejsantiagoh@users.noreply.github.com>
2024-11-08 21:20:55 -0500

    creación del menú
    Se crea un script con javascript y se trabaja en el index principal

---
+++ index.js
@@ -0,0 +1,19 @@
+const burgerbtn = document.querySelector("#burger");
+const navlinks = document.querySelector("#nav-links")
+burgerbtn.addEventListener("click", ()=>{
+    console.log("prueba")
+    navlinks.classList.toggle("is-active")
+    
+})
+
+
+
+  
+
+
+
+
+
+
+
+
